953294d52a16e0bdc82363db9b231596f8786cfa
LeeYongho91 <skyks4848@naver.com>
2023-10-16 23:17:19 +0900

    20231016

---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,39 @@
+<script setup>
+import Card from '~/components/Car/Card.client.vue';
+
+const route = useRoute();
+const {cars} = useCars();
+const { toTitleCase } = useUtilities();
+
+useHead({
+  title: toTitleCase(route.params.name)
+});
+
+const car = computed(() => {
+  return cars.find((c) => {
+    return c.id === parseInt(route.params.id);
+  })
+})
+
+if(!car.value) {
+  throw createError({
+    statusCode: 404,
+    message: `Car with id of ${route.params.id} does not exist`
+  })
+}
+
+definePageMeta({
+  layout: "custom"
+})
+
+
+
+</script>
+<template>
+  <div v-if="car">
+      <CarDetailHero :car="car" />
+      <CarDetailAttributes :features="car.features" />
+      <CarDetailDescription :description="car.description"/>
+      <CarDetailContact />
+  </div>
+</template>
---
+++ pages/city/[city]/car/hello.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>fjdaiosfjaois</h1>
+  </div>
+</template>
