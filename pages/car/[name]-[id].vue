66ba4c89d77f4cd32a3602d57676749ac7f572af
LeeYongho91 <skyks4848@naver.com>
2023-10-27 18:17:27 +0900

    prisma, supabase 테이블 추가

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -4,6 +4,7 @@
 const route = useRoute();
 const { data: car } = await useFetchCar(route.params.id); 
 const { toTitleCase } = useUtilities();
+const user = useSupabaseUser()
 
 useHead({
   title: toTitleCase(route.params.name)
@@ -20,6 +21,7 @@ definePageMeta({
 </script>
 <template>
   <div v-if="car">
+    {{ user.id }}
       <CarDetailHero :car="car" />
       <CarDetailAttributes :features="car.features" />
       <CarDetailDescription :description="car.description"/>
